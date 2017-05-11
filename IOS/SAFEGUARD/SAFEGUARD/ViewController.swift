aad5f473b2fd22e0a8d4f65f973e97da774c1bc1
kirankunigiri <kirankunigiri@gmail.com>
2017-05-11 14:28:27 -0700

    Added iOS design documents and created Xcode project
    Added the Sketch file with the complete design along with other design
    documents. An Xcode project was also created.

---
+++ IOS/SAFEGUARD/SAFEGUARD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAFEGUARD
+//
+//  Created by Kiran Kunigiri on 5/11/17.
+//  Copyright © 2017 Tinovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
